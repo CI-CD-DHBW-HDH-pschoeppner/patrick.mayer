fad97cd74cb17f119cc031ff73e09259793b6900
iOsShadow <77612730+iOsShadow@users.noreply.github.com>
2023-03-21 11:56:33 +0100

    bugfix

--- src/lib/TodoList.svelte
+++ src/lib/TodoList.svelte
@@ -7,7 +7,7 @@
   $: filteredTodos = todos.filter((todo) => {
     // TODO: es sollen nur die Items zurückgegeben werden, die den searchString (case insensitive) enthalten
     // Hier muss ein boolean zurückgegeben werden, der bestimmt, ob das Item (todo) angezeigt wird, oder nicht
-    return todo.value.toLowerCase().includes(searchString);
+    return todo.value.toLowerCase().includes(searchString.toLowerCase());
   });
 
   let searchString = "";
