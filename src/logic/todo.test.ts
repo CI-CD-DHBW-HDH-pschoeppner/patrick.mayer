b9c99da17c7b8d741523a784dfdc5bdd3b2841f3
iOsShadow <77612730+iOsShadow@users.noreply.github.com>
2023-03-21 11:51:22 +0100

    adjust prettier

--- src/logic/todo.test.ts
+++ src/logic/todo.test.ts
@@ -1,113 +1,138 @@
-import { formatTodo, generateColor, generateID, TodoItem, validateTodo } from "./todo";
-
+import {
+  formatTodo,
+  generateColor,
+  generateID,
+  TodoItem,
+  validateTodo,
+} from "./todo";
 
 describe("generate id", () => {
-    it("generated id is random", () => {
-      const result1 : string = generateID();
-      const result2 : string = generateID();
-      expect(result1 === result2).toBe(false);
-    });
+  it("generated id is random", () => {
+    const result1: string = generateID();
+    const result2: string = generateID();
+    expect(result1 === result2).toBe(false);
   });
+});
 
 describe("validate todo", () => {
-    const todoList:TodoItem[] = [{
-        id: "2h5bs",
-        value: "Hund Gassi gehen",
-        done:false
-    }];
+  const todoList: TodoItem[] = [
+    {
+      id: "2h5bs",
+      value: "Hund Gassi gehen",
+      done: false,
+    },
+  ];
 
-    it("value > 255 returns false", () => {
-        const result = validateTodo({id: "2h5bs",
-        value: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimatae",
-        done:false},todoList)
-        expect(result).toBe(false);
-    });
-    it("value < 255 returns true", () => {
-        const result = validateTodo({id: "2h5bs",
-        value: "Test",
-        done:false},todoList)
-        expect(result).toBe(true);
-    });
-    it("value = 255 returns true", () => {
-        const result = validateTodo({id: "2h5bs",
-        value: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata",
-        done:false},todoList)
-        expect(result).toBe(true);
-    });
-    it("value empty returns false", () => {
-        const result = validateTodo({id: "2h5bs",
-        value:"",
-        done:false},todoList)
-        expect(result).toBe(false);
-    });
-    it("value not empty returns true", () => {
-        const result = validateTodo({id: "2h5bs",
-        value:"a",
-        done:false},todoList)
-        expect(result).toBe(true);
-    });
-    it("todo (casesensitive) already in todolist returns false", () => {
-        const result = validateTodo({id: "2h5bs",
-        value:"Hund Gassi gehen",
-        done:false},todoList)
-        expect(result).toBe(false);
-    });
-    it("todo (caseinsensitive) already in todolist returns false", () => {
-        const result = validateTodo({id: "2h5bs",
-        value:"hund gassi gehen",
-        done:false},todoList)
-        expect(result).toBe(false);
-    });
-    it("todo not in todolist returns true", () => {
-        const result = validateTodo({id: "2h5bs",
-        value:"hund des Nachbarn gassi gehen",
-        done:false},todoList)
-        expect(result).toBe(true);
-    });
+  it("value > 255 returns false", () => {
+    const result = validateTodo(
+      {
+        id: "2h5bs",
+        value:
+          "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimatae",
+        done: false,
+      },
+      todoList
+    );
+    expect(result).toBe(false);
+  });
+  it("value < 255 returns true", () => {
+    const result = validateTodo(
+      { id: "2h5bs", value: "Test", done: false },
+      todoList
+    );
+    expect(result).toBe(true);
+  });
+  it("value = 255 returns true", () => {
+    const result = validateTodo(
+      {
+        id: "2h5bs",
+        value:
+          "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata",
+        done: false,
+      },
+      todoList
+    );
+    expect(result).toBe(true);
+  });
+  it("value empty returns false", () => {
+    const result = validateTodo(
+      { id: "2h5bs", value: "", done: false },
+      todoList
+    );
+    expect(result).toBe(false);
+  });
+  it("value not empty returns true", () => {
+    const result = validateTodo(
+      { id: "2h5bs", value: "a", done: false },
+      todoList
+    );
+    expect(result).toBe(true);
+  });
+  it("todo (casesensitive) already in todolist returns false", () => {
+    const result = validateTodo(
+      { id: "2h5bs", value: "Hund Gassi gehen", done: false },
+      todoList
+    );
+    expect(result).toBe(false);
+  });
+  it("todo (caseinsensitive) already in todolist returns false", () => {
+    const result = validateTodo(
+      { id: "2h5bs", value: "hund gassi gehen", done: false },
+      todoList
+    );
+    expect(result).toBe(false);
+  });
+  it("todo not in todolist returns true", () => {
+    const result = validateTodo(
+      { id: "2h5bs", value: "hund des Nachbarn gassi gehen", done: false },
+      todoList
+    );
+    expect(result).toBe(true);
+  });
 });
 
 describe("format todo", () => {
-    it("first letter is not capitalized yet", () => {
-      const result = formatTodo({
-        id: "e3j3d",
-        value:"hund Gassi gehen",
-        done: false
-    }).value
-      expect(result).toBe("Hund Gassi gehen");
-    });
-    it("first letter is already capitalized", () => {
-        const result = formatTodo({
-            id: "e3j3d",
-            value:"Hund Gassi gehen",
-            done: false
-        }).value
-          expect(result).toBe("Hund Gassi gehen");
-      });
+  it("first letter is not capitalized yet", () => {
+    const result = formatTodo({
+      id: "e3j3d",
+      value: "hund Gassi gehen",
+      done: false,
+    }).value;
+    expect(result).toBe("Hund Gassi gehen");
   });
+  it("first letter is already capitalized", () => {
+    const result = formatTodo({
+      id: "e3j3d",
+      value: "Hund Gassi gehen",
+      done: false,
+    }).value;
+    expect(result).toBe("Hund Gassi gehen");
+  });
+});
 
 describe("generate color", () => {
-    it("all values are between 50 and 150", () => {
-        const result = generateColor();
-        const split = result.split(",");    
-        const r = Number(split[0].slice(4));
-        const g = Number(split[1]);
-        const b = Number(split[2].split(")")[0]);
-        let boolresult = true;
-        if(!((50<=r)&&(r<=150))){
-            boolresult = false
-        }
-        if(!((50<=g) && (g<=150))){
-            boolresult = false
-        }
-        if(!((50<=b) && (b<=150))){
-            boolresult = false
-        }
-        expect(boolresult).toBe(true);
-    });
+  it("all values are between 50 and 150", () => {
+    const result = generateColor();
+    const split = result.split(",");
+    const r = Number(split[0].slice(4));
+    const g = Number(split[1]);
+    const b = Number(split[2].split(")")[0]);
+    let boolresult = true;
+    if (!(50 <= r && r <= 150)) {
+      boolresult = false;
+    }
+    if (!(50 <= g && g <= 150)) {
+      boolresult = false;
+    }
+    if (!(50 <= b && b <= 150)) {
+      boolresult = false;
+    }
+    expect(boolresult).toBe(true);
+  });
 
-    it("colors are random", () => {
-        const result1 : string = generateColor();
-        const result2 : string = generateColor();
-        expect(result1 === result2).toBe(false);
-    });
+  it("colors are random", () => {
+    const result1: string = generateColor();
+    const result2: string = generateColor();
+    expect(result1 === result2).toBe(false);
+  });
 });
--- src/logic/todo.ts
+++ src/logic/todo.ts
@@ -8,12 +8,12 @@ export class TodoItem {
 
 // this function must return a unique ID every time it is called
 export function generateID(): string {
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    let result = "";
-    for (let i = 0; i < 5; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let result = "";
+  for (let i = 0; i < 5; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
 }
 
 // make sure, that
@@ -21,16 +21,16 @@ export function generateID(): string {
 // the value isn' empty
 // the todo isn't contained in the todos array (case insensitive)
 export function validateTodo(todo: TodoItem, todos: TodoItem[]): boolean {
-  if(todo.value.length>255){
+  if (todo.value.length > 255) {
     return false;
   }
-  if(todo.value.length === 0){
+  if (todo.value.length === 0) {
     return false;
   }
-  let result = true
-  todos.forEach(element => {
-    if(element.value.toLowerCase() === todo.value.toLowerCase()){
-        result = false
+  let result = true;
+  todos.forEach((element) => {
+    if (element.value.toLowerCase() === todo.value.toLowerCase()) {
+      result = false;
     }
   });
   return result;
@@ -41,8 +41,8 @@ export function formatTodo(todo: TodoItem): TodoItem {
   return {
     id: todo.id,
     value: todo.value.charAt(0).toUpperCase() + todo.value.slice(1),
-    done: false
-  }
+    done: false,
+  };
 }
 
 // generate a random rgb color
@@ -51,7 +51,7 @@ export function generateColor(): string {
   const r = Math.floor(Math.random() * (150 - 50 + 1)) + 50;
   const g = Math.floor(Math.random() * (150 - 50 + 1)) + 50;
   const b = Math.floor(Math.random() * (150 - 50 + 1)) + 50;
-  return `rgb(${r.toString()},${g.toString()},${b.toString()})`
+  return `rgb(${r.toString()},${g.toString()},${b.toString()})`;
 }
 
 export const todoList = writable<TodoItem[]>([]);
